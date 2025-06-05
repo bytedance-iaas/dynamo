199c92d0070bb39ba88b5516bd1accf4af6fd7d0
ShawnKung <gyx215@outlook.com>
2025-06-05 15:01:54 +0800

    build(scm): support build wheel only && change apt ubuntu source to byted

--- container/build_scm.sh
+++ container/build_scm.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-# 打包时必填字段: CUSTOM_VLLM_REF, CUSTOM_VLLM_WHEEL_REPO, CUSTOM_TARGET, CUSTOM_DOCKER_USERNAME, CUSTOM_DOCKER_PASSWORD
+# 打包时必填字段: CUSTOM_VLLM_REF, CUSTOM_VLLM_WHEEL_REPO, CUSTOM_DOCKER_USERNAME, CUSTOM_DOCKER_PASSWORD
 
 ROOT_DIR=$(pwd)
 OUTPUT_DIR=${ROOT_DIR}/output
@@ -29,30 +29,44 @@ if [ -z "${CUSTOM_VLLM_WHEEL_REPO}" ]; then
 fi
 VLLM_WHEEL_REPO=${CUSTOM_VLLM_WHEEL_REPO}
 
-TARGET=""
-if [ ! -z "${CUSTOM_TARGET}" ]; then
-    TARGET=${CUSTOM_TARGET}
+TARGET="runtime"
+IMAGE_REPO_NAME="dynamo"
+
+if [ "${CUSTOM_BUILD_WHEEL_ONLY}" == "true" ]; then
+    # 如果 CUSTOM_BUILD_WHEEL_ONLY 为 true，则只构建 wheelhouse 镜像，并随后上传到 tos
+    TARGET="wheel_builder"
+    IMAGE_REPO_NAME="wheelhouse"
+    # tos 相关参数，必须设置
+    if [ -z "$CUSTOM_TOS_AK" ] && [ -z "$CUSTOM_TOS_SK" ]; then
+        echo "CUSTOM_TOS_AK and CUSTOM_TOS_SK are not set"
+        exit 1
+    fi
+    if [ -z "$CUSTOM_TOS_BUCKET" ]; then
+        echo "CUSTOM_TOS_BUCKET is not set"
+        exit 1
+    fi
+else
+    # 默认构建 runtime 镜像，并随后 push 到 cr
+    # docker push username/password，必须设置
+    if [ -z "${CUSTOM_DOCKER_USERNAME}" ] || [ -z "${CUSTOM_DOCKER_PASSWORD}" ]; then
+        echo "CUSTOM_DOCKER_USERNAME or CUSTOM_DOCKER_PASSWORD is not set"
+        exit 1
+    fi
+    # 检查 IMAGE_TAG，应当包含 .byted 字符串
+    if [[ "${IMAGE_TAG}" != *".byted"* ]]; then
+        echo "IMAGE_TAG must contain .byted string, please check CUSTOM_VLLM_REF env"
+        exit 1
+    fi
+
+    docker login -u ${CUSTOM_DOCKER_USERNAME} -p ${CUSTOM_DOCKER_PASSWORD} iaas-gpu-cn-beijing.cr.volces.com
 fi
 
 # IMAGE_NAME 格式：dynamo:v0.8.5.byted.0.0.2.202505152017
 IMAGE_TAG=$(echo v${VLLM_REF#v} | sed 's/+/./g')
 IMAGE_TAG=$(echo "$IMAGE_TAG" | sed "s/\(.*\.\)[0-9]\+/\1$BUILD_TIME/")  # 替换为当前时戳
-IMAGE_NAME=dynamo:${IMAGE_TAG}
-TARGET_IMAGE=iaas-gpu-cn-beijing.cr.volces.com/serving/dynamo:${IMAGE_TAG}
+IMAGE_NAME=${IMAGE_REPO_NAME}:${IMAGE_TAG}
+TARGET_IMAGE=iaas-gpu-cn-beijing.cr.volces.com/serving/${IMAGE_NAME}
 
-# 检查 IMAGE_TAG，应当包含 .byted 字符串
-if [[ "${IMAGE_TAG}" != *".byted"* ]]; then
-    echo "IMAGE_TAG must contain .byted string, please check CUSTOM_VLLM_REF env"
-    exit 1
-fi
-
-# docker push username/password，必须设置
-if [ -z "${CUSTOM_DOCKER_USERNAME}" ] || [ -z "${CUSTOM_DOCKER_PASSWORD}" ]; then
-    echo "CUSTOM_DOCKER_USERNAME or CUSTOM_DOCKER_PASSWORD is not set"
-    exit 1
-fi
-
-docker login -u ${CUSTOM_DOCKER_USERNAME} -p ${CUSTOM_DOCKER_PASSWORD} iaas-gpu-cn-beijing.cr.volces.com
 
 # 如果是 SCM 构建，则准备 docker 环境
 if [[ "${SCM_BUILD}" == "True" ]]; then
@@ -65,6 +79,9 @@ sed -i 's|docker build -f|docker build --network=host -f|' ./build_submodule.sh
 sed -i 's|VLLM_BASE_IMAGE="nvcr.io/nvidia/cuda-dl-base"|VLLM_BASE_IMAGE="iaas-gpu-cn-beijing.cr.volces.com/nvcr.io/nvidia/cuda-dl-base"|' ./build_submodule.sh
 sed -i 's|ARG BASE_IMAGE="nvcr.io/nvidia/cuda-dl-base"|ARG BASE_IMAGE="iaas-gpu-cn-beijing.cr.volces.com/nvcr.io/nvidia/cuda-dl-base"|' ./Dockerfile.vllm.submodule
 sed -i 's|ARG RUNTIME_IMAGE="nvcr.io/nvidia/cuda"|ARG RUNTIME_IMAGE="iaas-gpu-cn-beijing.cr.volces.com/nvcr.io/nvidia/cuda"|' ./Dockerfile.vllm.submodule
+# 由于 ubuntu 官方源不稳定，使用 byted 源 
+sed -i 's@RUN apt-get update@RUN sed -i "s|http://archive.ubuntu.com|http://mirrors.byted.org|g" /etc/apt/sources.list.d/ubuntu.sources ; sed -i "s|http://security.ubuntu.com|http://mirrors.byted.org|g" /etc/apt/sources.list.d/ubuntu.sources ; apt-get update@' ./Dockerfile.vllm.submodule
+sed -i 's@RUN apt update@RUN sed -i "s|http://archive.ubuntu.com|http://mirrors.byted.org|g" /etc/apt/sources.list.d/ubuntu.sources ; sed -i "s|http://security.ubuntu.com|http://mirrors.byted.org|g" /etc/apt/sources.list.d/ubuntu.sources ; apt update@' ./Dockerfile.vllm.submodule
 
 proxy_args=""
 if [ ! -z "$http_proxy" ]; then
@@ -87,15 +104,31 @@ if [ ! -z "${CUSTOM_HPKV_VERSION}" ]; then
     hpkv_version_arg="--build-arg HPKV_VERSION=${CUSTOM_HPKV_VERSION}"
 fi
 
-# 构建 submodule
+# 构建 submodule 或 wheelhouse， 取决于 CUSTOM_BUILD_WHEEL_ONLY 是否为 true
 ./build_submodule.sh ${hpkv_version_arg} --build-arg VLLM_PATCHED_PACKAGE_NAME=vllm --build-arg VLLM_WHEEL_REPO=${VLLM_WHEEL_REPO} --build-arg VLLM_REF=${VLLM_REF} --build-arg VLLM_PATCHED_PACKAGE_VERSION=${VLLM_VERSION} ${proxy_args} --build-arg CARGO_BUILD_JOBS=$(nproc) ${target_args} --tag ${IMAGE_NAME}
 
-docker images
-docker tag ${IMAGE_NAME} ${TARGET_IMAGE}
-docker push ${TARGET_IMAGE}
-
-echo "Build and push ${TARGET_IMAGE} successfully"
-
-cd ${ROOT_DIR}
 mkdir -p ${OUTPUT_DIR}
-echo ${TARGET_IMAGE} > ${OUTPUT_DIR}/image_name
+
+if [ "${CUSTOM_BUILD_WHEEL_ONLY}" == "true" ]; then
+    echo "Build wheelhouse image ${IMAGE_NAME} successfully"
+    docker run --rm -v ${OUTPUT_DIR}:${OUTPUT_DIR} ${IMAGE_NAME} bash -c 'ls /workspace/dist && for file in $(find /workspace/dist -name "ai_dynamo*.whl"); do cp -v $file '"${OUTPUT_DIR}"'; done'
+    echo "Upload wheels to tos"
+    TOS_UTIL_URL=https://tos-tools.tos-cn-beijing.volces.com/linux/amd64/tosutil
+    if [ ! -z "$CUSTOM_TOS_UTIL_URL" ]; then
+        TOS_UTIL_URL=$CUSTOM_TOS_UTIL_URL
+    fi
+    wget $TOS_UTIL_URL -O tosutil && chmod +x tosutil
+    for wheel_file in $(find $OUTPUT_DIR -name "*.whl"); do
+        echo "uploading $wheel_file to tos..."
+        ./tosutil cp $wheel_file tos://${CUSTOM_TOS_BUCKET}/packages/$(basename $wheel_file) -re cn-beijing -e tos-cn-beijing.volces.com -i $CUSTOM_TOS_AK -k $CUSTOM_TOS_SK
+    done
+    echo "Upload wheels to tos successfully"
+else
+    docker images
+    docker tag ${IMAGE_NAME} ${TARGET_IMAGE}
+    docker push ${TARGET_IMAGE}
+
+    echo "Build and push ${TARGET_IMAGE} successfully"
+    cd ${ROOT_DIR}
+    echo ${TARGET_IMAGE} > ${OUTPUT_DIR}/image_name
+fi
